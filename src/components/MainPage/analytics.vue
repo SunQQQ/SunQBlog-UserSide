16a5a594dcf94e51190372b48c33b034e8fd37e1
sunquan <1585437938@qq.com>
2021-11-29 11:03:10 +0800

    修改中国地图、折线图配色为网站主色调

--- src/components/MainPage/analytics.vue
+++ src/components/MainPage/analytics.vue
@@ -106,251 +106,261 @@
 </template>
 
 <script>
-  import Store from "../../store";
-  import china from '../../static/map/china.json'
-  import citys from '../../static/map/citys'
+import Store from "../../store";
+import china from '../../static/map/china.json'
+import citys from '../../static/map/citys'
 
-  export default {
-    name: "analytics",
-    data: function () {
-      return {
-        todayVisit: 0,
-        yesterdayVisit: 0,
-        allVisitNum: 0,
-        weekVisit: 0,
-        // 折线图数据
-        lineChartOption: {
-          // title: {text: '数据趋势'},
-          tooltip: {
-            trigger: 'axis'
-          },
-          legend: {
-            type: 'plain'
-          },
-          xAxis: {data: []},
-          yAxis: {},
-          series: [{
-            name: '博客访问量(人/天)', type: 'line', data: [],
-            itemStyle: {normal: {label: {show: true}}}
-          }],
-          grid: {
-            bottom: '20px' // 图表距离容器下方边距
-          }
+export default {
+  name: "analytics",
+  data: function () {
+    return {
+      todayVisit: 0,
+      yesterdayVisit: 0,
+      allVisitNum: 0,
+      weekVisit: 0,
+      // 折线图数据
+      lineChartOption: {
+        // title: {text: '数据趋势'},
+        tooltip: {
+          trigger: 'axis'
         },
-        visitListData: [],
-        // 地图参数
-        mapList: [],
-        mapOption: {
-          legend:{
-            data:['博客访问来源']
-          },
-          tooltip: {
-            show: false
-          },
-          geo: {
-            map: "china",
-            roam: false,// 一定要关闭拖拽
-            zoom: 1.7,
-            center: [105, 36], // 调整地图位置
-            showLegendSymbol: false, // 存在legend时显示
-            label: {
-              normal: {
-                show: false
-              },
-              emphasis: {
-                show: true,
-                textStyle: {
-                  color: "#000000"
-                }
-              }
+        legend: {
+          type: 'plain'
+        },
+        xAxis: {data: []},
+        yAxis: {},
+        series: [{
+          name: '博客访问量(人/天)', type: 'line', data: [],
+          itemStyle: {
+            normal: {
+              color:'#01aaed',
+              label: {show: true},
+              lineStyle: {color: '#01aaed'}
+            }
+          }
+        }],
+        grid: {
+          bottom: '20px' // 图表距离容器下方边距
+        }
+      },
+      visitListData: [],
+      // 地图参数
+      mapList: [],
+      mapOption: {
+        legend: {
+          data: ['博客访问来源']
+        },
+        tooltip: {
+          show: false
+        },
+        geo: {
+          map: "china",
+          roam: false,// 一定要关闭拖拽
+          zoom: 1.7,
+          center: [105, 36], // 调整地图位置
+          showLegendSymbol: false, // 存在legend时显示
+          label: {
+            normal: {
+              show: false
             },
-            itemStyle: {
-              normal: {
-                areaColor: "white",
-                borderColor: "#aeaeae",
-                borderWidth: 0.5
-              },
-              emphasis: {
-                areaColor: "#cfcddb",
-                shadowOffsetX: 0,
-                shadowOffsetY: 0,
-                shadowBlur: 5,
-                borderWidth: 0
+            emphasis: {
+              show: true,
+              textStyle: {
+                color: "#000000"
               }
             }
           },
-          series: [{
-            name: '博客访问来源', type: 'scatter', data: [],
-            itemStyle: {
-              shadowBlur: 0,
-              shadowColor: '#333'
+          itemStyle: {
+            normal: {
+              areaColor: "white",
+              borderColor: "#aeaeae",
+              borderWidth: 0.5
             },
-            coordinateSystem: 'geo',
-            symbolSize: 5,
-            label: {
-              formatter: '{b}',
-              position: 'right',
-              show: true
-            },
+            emphasis: {
+              areaColor: "#f6f6f6",
+              shadowOffsetX: 0,
+              shadowOffsetY: 0,
+              shadowBlur: 5,
+              borderWidth: 0
+            }
           }
-          ]
+        },
+        series: [{
+          name: '博客访问来源', type: 'scatter', data: [],
+          itemStyle: {
+            shadowBlur: 0,
+            shadowColor: '#333',
+            normal: {
+              color:'#01aaed',
+              lineStyle: {color: '#01aaed'}
+            }
+          },
+          coordinateSystem: 'geo',
+          symbolSize: 5,
+          label: {
+            formatter: '{b}',
+            position: 'right',
+            show: true
+          },
         }
+        ]
       }
-    },
-    methods: {
-      // 渲染折线图
-      setLineChart: function () {
-        let that = this,
-          totalVisit = 0,
-          lineChart = that.$echarts.init(document.getElementById('line-chart'));
-        this.SQFrontAjax({
-          Url: '/api/visitCount/foreend',
-          UploadData: {
-            endTime: this.getSQTime().split(' ')[0],
-            dayNum: 7
-          },
-          Success: function (data) {
-            that.todayVisit = data.dateCountList[0].reading;
-            that.yesterdayVisit = data.dateCountList[1].reading;
-
-            let dates = [], readings = [];
-            data.dateCountList.forEach(function (item) {
-              dates.push(item.time);
-              readings.push(item.reading);
-              totalVisit += item.reading;
-            });
-            that.lineChartOption.xAxis.data = dates.reverse();
-            that.lineChartOption.series[0].data = readings.reverse();
-            that.weekVisit = totalVisit;
-            lineChart.setOption(that.lineChartOption);
-          }
-        });
-      },
-      // 渲染列表
-      setVisitList: function () {
-        let that = this;
-        this.SQFrontAjax({
-          Url: '/api/visitRead/foreend',
-          UploadData: {},
-          Success: function (data) {
-            that.allVisitNum = data.length;
-            data.forEach(function (item) {
-              if (JSON.stringify(item.location) == '[]') item.location = '银河系';
-              if (!item.browser) item.browser = "secret";
-              if (!item.fromUrl) item.fromUrl = '直接访问';
-            });
-            that.visitListData = data;
-          }
-        });
-      },
-      // 渲染地图
-      setMap: function () {
-        let map, that = this;
-
-        this.SQFrontAjax({
-          Url: '/api/visitCount/foreend',
-          UploadData: {
-            endTime: this.getSQTime().split(' ')[0],
-            dayNum: 1
-          },
-          Success: function (data) {
-            data.dateList.forEach(function (item, i) {
-              let location = item.location, formatLocation;
-              if (typeof (location) == 'string') {
-                formatLocation = location.replace('市', ''); // 数据库存放的城市名称包含'市'字，但是所有城市维度数据中城市名称没有
-                if (citys[formatLocation]) {
-                  that.mapList.push({
-                    name: formatLocation,
-                    value: citys[formatLocation]
-                  });
-                }
-              }
-            });
-
-            console.log('that.mapList', that.mapList);
-
-            that.mapOption.series[0].data = that.mapList;
-
-            map = that.$echarts.init(document.getElementById('map'));
-            that.$echarts.registerMap('china', china);
-            map.setOption(that.mapOption);
-          }
-        });
-      }
-    },
-    mounted: function () {
-      Store.commit("ChangeActive", 5);// 切换Topbar高亮
-
-      this.setLineChart();
-      this.setVisitList();
-      this.setMap();
     }
+  },
+  methods: {
+    // 渲染折线图
+    setLineChart: function () {
+      let that = this,
+        totalVisit = 0,
+        lineChart = that.$echarts.init(document.getElementById('line-chart'));
+      this.SQFrontAjax({
+        Url: '/api/visitCount/foreend',
+        UploadData: {
+          endTime: this.getSQTime().split(' ')[0],
+          dayNum: 7
+        },
+        Success: function (data) {
+          that.todayVisit = data.dateCountList[0].reading;
+          that.yesterdayVisit = data.dateCountList[1].reading;
+
+          let dates = [], readings = [];
+          data.dateCountList.forEach(function (item) {
+            dates.push(item.time);
+            readings.push(item.reading);
+            totalVisit += item.reading;
+          });
+          that.lineChartOption.xAxis.data = dates.reverse();
+          that.lineChartOption.series[0].data = readings.reverse();
+          that.weekVisit = totalVisit;
+          lineChart.setOption(that.lineChartOption);
+        }
+      });
+    },
+    // 渲染列表
+    setVisitList: function () {
+      let that = this;
+      this.SQFrontAjax({
+        Url: '/api/visitRead/foreend',
+        UploadData: {},
+        Success: function (data) {
+          that.allVisitNum = data.length;
+          data.forEach(function (item) {
+            if (JSON.stringify(item.location) == '[]') item.location = '银河系';
+            if (!item.browser) item.browser = "secret";
+            if (!item.fromUrl) item.fromUrl = '直接访问';
+          });
+          that.visitListData = data;
+        }
+      });
+    },
+    // 渲染地图
+    setMap: function () {
+      let map, that = this;
+
+      this.SQFrontAjax({
+        Url: '/api/visitCount/foreend',
+        UploadData: {
+          endTime: this.getSQTime().split(' ')[0],
+          dayNum: 1
+        },
+        Success: function (data) {
+          data.dateList.forEach(function (item, i) {
+            let location = item.location, formatLocation;
+            if (typeof (location) == 'string') {
+              formatLocation = location.replace('市', ''); // 数据库存放的城市名称包含'市'字，但是所有城市维度数据中城市名称没有
+              if (citys[formatLocation]) {
+                that.mapList.push({
+                  name: formatLocation,
+                  value: citys[formatLocation]
+                });
+              }
+            }
+          });
+
+          console.log('that.mapList', that.mapList);
+
+          that.mapOption.series[0].data = that.mapList;
+
+          map = that.$echarts.init(document.getElementById('map'));
+          that.$echarts.registerMap('china', china);
+          map.setOption(that.mapOption);
+        }
+      });
+    }
+  },
+  mounted: function () {
+    Store.commit("ChangeActive", 5);// 切换Topbar高亮
+
+    this.setLineChart();
+    this.setVisitList();
+    this.setMap();
   }
+}
 </script>
 
 <style scoped lang="less">
-  @import "../../static/css/base";
-  @import "../../static/css/AboutMe";
+@import "../../static/css/base";
+@import "../../static/css/AboutMe";
 
-  .quota-content {
-    .myflex(center);
-    color: rgba(0, 0, 0, 0.65);
-  }
+.quota-content {
+  .myflex(center);
+  color: rgba(0, 0, 0, 0.65);
+}
 
-  .quota-item {
-    flex: 1;
-    text-align: center;
-  }
+.quota-item {
+  flex: 1;
+  text-align: center;
+}
 
-  .quota-item .num {
-    font-size: 1.2rem;
-    margin-bottom: 0;
-  }
+.quota-item .num {
+  font-size: 1.2rem;
+  margin-bottom: 0;
+}
 
-  .block {
-    background-color: #FFFFFF;
-    margin-top: 1rem;
-    padding: 1rem;
-    border-radius: 2px;
-  }
+.block {
+  background-color: #FFFFFF;
+  margin-top: 1rem;
+  padding: 1rem;
+  border-radius: 2px;
+}
 
-  .block-name {
-    padding: 0.5rem;
-    border-bottom: 1px solid #f0f0f0;
-  }
+.block-name {
+  padding: 0.5rem;
+  border-bottom: 1px solid #f0f0f0;
+}
 
-  .line-chart {
-    height: 300px;
-  }
+.line-chart {
+  height: 300px;
+}
 
-  .map-chart {
-    height: 400px;
-  }
+.map-chart {
+  height: 400px;
+}
 
-  .list-head {
-    color: #8590a6;
-    .myflex(center);
-    border-bottom: 1px solid #f0f0f0;
-    padding: 8px 0;
-  }
+.list-head {
+  color: #8590a6;
+  .myflex(center);
+  border-bottom: 1px solid #f0f0f0;
+  padding: 8px 0;
+}
 
-  .list-tr {
-    .myflex(center);
-    //border-bottom: 1px solid #f0f0f0;
-    padding: 8px 0;
-  }
+.list-tr {
+  .myflex(center);
+  //border-bottom: 1px solid #f0f0f0;
+  padding: 8px 0;
+}
 
-  .list-td {
-    flex: 1;
-    padding-left: 1rem;
-  }
+.list-td {
+  flex: 1;
+  padding-left: 1rem;
+}
 
-  .list .single {
-    background: #f6f6f6;
-  }
+.list .single {
+  background: #f6f6f6;
+}
 
-  .list .align {
-    text-align: right;
-    padding-right: 1rem;
-  }
+.list .align {
+  text-align: right;
+  padding-right: 1rem;
+}
 </style>
