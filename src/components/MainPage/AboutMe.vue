cbe8b6bfdee6d5a7f48510b53a3f0b7983936667
sunquan <1585437938@qq.com>
2022-09-10 10:08:35 +0800

    优化关于我页面

--- src/components/MainPage/AboutMe.vue
+++ src/components/MainPage/AboutMe.vue
@@ -4,6 +4,26 @@
       <div class="FlexContent">
         <div class="LeftPart">
           <div class="BigBlock main-text">
+            <div class="TitleFontLine">博客特色</div>
+            <p><i class="iconfont icon-person AboutMeIcon" style="color:#49a9ee"></i>本博客不强制游客注册/登录，留言/评论等功能只需填入昵称，即可使用。</p>
+            <p><i class="iconfont AboutMeIcon icon-github" style="color:#948aec"></i>博客对外开放管理后台、流量数据、所有源码等，分享技术，毫无保留。</p>
+            <p><i class="iconfont AboutMeIcon icon-fuwuqi" style="color:#f78e3d"></i>博客用户端、管理后台、后端接口、UI设计、服务器部署全部由一人维护。持续优化，随时更新。</p>
+            <p><i class="iconfont icon-tool AboutMeIcon" style="color:#3db8c1"></i>已实现各种父子组件、数据缓存、防抖节流、用户轨迹、首屏优化、访问数据分析等功能。</p>
+            <p><i class="iconfont icon-aihao1 AboutMeIcon" style="color:#f46e65"></i>跪求Github一赞，点个星星将是对开源开发者的最大支持与鼓励！</p>
+          </div>
+        
+          <div class="BigBlock AboutMeMarginTop main-text">
+            <div class="TitleFontLine">博客技术</div>
+            <p><i class="iconfont AboutMeIcon icon-wangzhanxinxi" style="color:#f46e65"></i>本博客纯手工编写，未使用第三方建站或流量统计工具。代码开源，欢迎匡正</p>
+            <p><i class="iconfont AboutMeIcon icon-quanju_qianduanjiemian" style="color:#3dbd7d"></i>用户端手写的UI组件 + Vue.js，管理端使用ElementUI + Vue.js</p>
+            <p><i class="iconfont AboutMeIcon icon-fuwuqi" style="color:#f78e3d"></i>服务端语言使用node.js、框架使用Express，博客部署在腾讯云、使用Linux服务器</p>
+            <p><i class="iconfont AboutMeIcon icon-dataBase" style="color:#49a9ee"></i>数据库使用MongoDB，可视化工具用的Robo 3T</p>
+            <p>
+              <i class="iconfont AboutMeIcon icon-github" style="color:#948aec"></i>用户端/管理端/后端代码：<a href="https://github.com/SunQQQ" target="_blank">GitHub地址</a>。整套代码都在这了
+            </p>
+          </div>
+
+          <div class="BigBlock AboutMeMarginTop main-text">
             <div class="TitleFontLine">关于博主</div>
             <p><i class="iconfont icon-person AboutMeIcon" style="color:#49a9ee"></i>博主名字叫孙权，94年人，软件工程本科毕业，从事前端开发岗位。
             </p>
@@ -13,24 +33,7 @@
             <p><i class="iconfont icon-xiazai20 AboutMeIcon" style="color:#948aec"></i>家庭住址：郑州市 二七区 南三环</p>
             <p><i class="iconfont icon-weixin AboutMeIcon" style="color:#f78e3d"></i>微信：13213074006，欢迎商业合作与技术交流</p>
           </div>
-          <div class="BigBlock AboutMeMarginTop main-text">
-            <div class="TitleFontLine">博客技术</div>
-            <p><i class="iconfont AboutMeIcon icon-wangzhanxinxi" style="color:#f46e65"></i>本博客纯手工编写，未使用第三方建站或流量统计工具。代码开源，欢迎匡正</p>
-            <p><i class="iconfont AboutMeIcon icon-quanju_qianduanjiemian" style="color:#3dbd7d"></i>用户端手写的UI组件 + Vue.js，管理端使用ElementUI + Vue.js</p>
-            <p><i class="iconfont AboutMeIcon icon-fuwuqi" style="color:#f78e3d"></i>服务端语言使用node.js、框架使用Express，博客部署在腾讯云、使用Linux服务器</p>
-            <p>
-              <i class="iconfont AboutMeIcon icon-github" style="color:#948aec"></i>用户端/管理端/后端代码：<a href="https://github.com/SunQQQ" target="_blank">GitHub地址</a>。每看到一个赞，俺都开心的像个孩子！
-            </p>
-            <p><i class="iconfont AboutMeIcon icon-dataBase" style="color:#49a9ee"></i>数据库使用MongoDB，可视化工具用的Robo 3T</p>
-          </div>
-          <div class="BigBlock AboutMeMarginTop main-text">
-            <div class="TitleFontLine">博客特色</div>
-            <p>1、本博客不强制游客注册/登录，留言、评论等功能只需填入昵称，即可使用。</p>
-            <p>2、博客对外开放管理后台、流量数据、所有源码等，分享技术，毫无保留。</p>
-            <p>3、博客用户端、管理后台、后端接口、UI设计、服务器部署全部由一人维护。持续优化，随时更新。</p>
-            <p>4、已实现异常处理、公共工具类、各种父子组件、数据缓存、防抖节流、用户轨迹、首屏优化等功能。</p>
-            <p>5、别的我想到了再来夸自己。这里跪求您的Github一赞，谢谢您对博客开发者的支持！</p>
-          </div>
+          
           <div class="BigBlock AboutMeMarginTop">
             <div class="TitleFontLine TitleFontLineThank">Thanks You All</div>
             <div class="UrlCardTr AboutPage">
