868009e77cb87f966c61a5f38903e97e5e53f33d
sunquan <1585437938@qq.com>
2023-04-11 16:18:37 +0800

    生产删除console/debugger

--- vue.config.js
+++ vue.config.js
@@ -27,6 +27,8 @@ module.exports = {
   configureWebpack:config=>{
     // GZip压缩
     const CompressionPlugin = require('compression-webpack-plugin');
+    // build时关闭console、debugger
+    const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
     config.plugins.push(
       new CompressionPlugin({
         algorithm:'gzip',
@@ -34,6 +36,17 @@ module.exports = {
         threshold:10240, // 压缩超过10k的数据
         deleteOriginalAssets:false, // 不删除压缩前的文件，如果浏览器不支持Gzip，则会加载源文件
         minRatio:0.8 // 压缩比大于0.8的文件将不会被压缩
+      })
+    );
+
+    config.plugins.push(
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: {
+            drop_debugger: true,//关闭debug
+            drop_console: true,//关闭console
+          }
+        },
       })
     );
   }
